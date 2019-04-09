11c67e73d7b583ae6d1022552d1f79766984d830
Thoreex <14024927+Thoreex@users.noreply.github.com>
2019-04-09 16:57:26 -0600

    App test corregido

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Turismobook');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title in a a tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Turismobook!');
+    expect(compiled.querySelector('a').textContent).toContain('Turismobook');
   });
 });
