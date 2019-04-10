205d5d341d8392919ea5999c7252000b8db18640
nervincr <nervincr@users.noreply.github.com>
2019-04-10 04:06:50 -0600

    Se agrega import faltante a test de Auth.

--- src/app/auth/auth.guard.spec.ts
+++ src/app/auth/auth.guard.spec.ts
@@ -2,12 +2,13 @@ import { TestBed, async, inject } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 
 describe('AuthGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [AuthGuard],
-      imports: [RouterTestingModule]
+      imports: [RouterTestingModule, FormsModule]
     });
   });
 
