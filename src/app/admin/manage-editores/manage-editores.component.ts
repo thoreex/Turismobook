e2011df245c2427b2877d76a6cef6c4b3321e59f
nervincr <nervincr@users.noreply.github.com>
2019-05-24 01:12:00 -0600

    Se agrega componente ngx-select-dropdown.

--- src/app/admin/manage-editores/manage-editores.component.ts
+++ src/app/admin/manage-editores/manage-editores.component.ts
@@ -3,6 +3,7 @@ import { CentrosService } from 'src/app/centros/centros.service';
 import { UsuariosService } from 'src/app/usuarios/usuarios.service';
 import { Centro } from 'src/app/centros/centro';
 import { Usuario } from 'src/app/usuarios/usuario';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-editores',
@@ -13,7 +14,85 @@ export class ManageEditoresComponent implements OnInit {
   mensajeError: string;
   error: boolean;
 
-  constructor(private centrosSevice: CentrosService, private usuariosService: UsuariosService) { }
+  singleSelect: any = [];
+  multiSelect: any = [];
+  stringArray: any = [];
+  objectsArray: any = [];
+  stringOptions = [
+    'Burns Dalton', 'Mcintyre Lawson', 'Amie Franklin', 'Jocelyn Horton', 'Fischer Erickson', 'Medina Underwood', 'Goldie Barber'
+  ];
+  config = { displayKey: 'name', search: true, placeholder: 'pick one' };
+  selectedOptions = [{
+    _id: '5a66d6c31d5e4e36c7711b7a',
+    index: 0,
+    balance: '$2,806.37',
+    picture: 'http://placehold.it/32x32',
+    name: 'Burns Dalton'
+  },
+  {
+    _id: '5a66d6c3657e60c6073a2d22',
+    index: 1,
+    balance: '$2,984.98',
+    picture: 'http://placehold.it/32x32',
+    name: 'Mcintyre Lawson'
+  }];
+  options = [
+    {
+      _id: '5a66d6c31d5e4e36c7711b7a',
+      index: 0,
+      balance: '$2,806.37',
+      picture: 'http://placehold.it/32x32',
+      name: 'Burns Dalton'
+    },
+    {
+      _id: '5a66d6c3657e60c6073a2d22',
+      index: 1,
+      balance: '$2,984.98',
+      picture: 'http://placehold.it/32x32',
+      name: 'Mcintyre Lawson'
+    },
+    {
+      _id: '5a66d6c376be165a5a7fae33',
+      index: 2,
+      balance: '$2,794.16',
+      picture: 'http://placehold.it/32x32',
+      name: 'Amie Franklin'
+    },
+    {
+      _id: '5a66d6c3f7854b6b4d96333b',
+      index: 3,
+      balance: '$2,537.14',
+      picture: 'http://placehold.it/32x32',
+      name: 'Jocelyn Horton'
+    },
+    {
+      _id: '5a66d6c31f967d4f3e9d84e9',
+      index: 4,
+      balance: '$2,141.42',
+      picture: 'http://placehold.it/32x32',
+      name: 'Fischer Erickson'
+    },
+    {
+      _id: '5a66d6c34cfa8cddefb31602',
+      index: 5,
+      balance: '$1,398.60',
+      picture: 'http://placehold.it/32x32',
+      name: 'Medina Underwood'
+    },
+    {
+      _id: '5a66d6c3d727c450794226de',
+      index: 6,
+      balance: '$3,915.65',
+      picture: 'http://placehold.it/32x32',
+      name: 'Goldie Barber'
+    }
+  ];
+  selectForm: FormGroup;
+  constructor(private centrosSevice: CentrosService, private usuariosService: UsuariosService, private fromBuilder: FormBuilder) {
+    this.selectForm = this.fromBuilder.group({
+      selectDrop: [null, Validators.required]
+    });
+  }
 
   ngOnInit() {
   }
