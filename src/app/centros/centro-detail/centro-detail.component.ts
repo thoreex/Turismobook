79aa15a41389bd3b253de5a06bd1c6f520c18d6c
Thoreex <14024927+Thoreex@users.noreply.github.com>
2019-05-27 08:16:57 -0600

    Feature para seguir completada

--- src/app/centros/centro-detail/centro-detail.component.ts
+++ src/app/centros/centro-detail/centro-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { CentrosService } from '../centros.service';
 import { Centro } from '../centro';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-centro-detail',
@@ -14,7 +15,8 @@ export class CentroDetailComponent implements OnInit {
   dangerousVideoUrl: string;
   videoUrl: SafeResourceUrl;
 
-  constructor(private centrosService: CentrosService, private route: ActivatedRoute, private sanitizer: DomSanitizer) {
+  constructor(private centrosService: CentrosService, private authService: AuthService,
+              private route: ActivatedRoute, private sanitizer: DomSanitizer) {
   }
 
   ngOnInit() {
@@ -30,4 +32,38 @@ export class CentroDetailComponent implements OnInit {
       subscribe(centro => this.centro = centro);
   }
 
+  follow() {
+    // seguir/desseguir centro
+    const loggedUser = this.authService.oUsuario;
+    if (loggedUser) {
+      // en caso de seguidores ser undefined
+      if (!loggedUser.seguidores) {
+        loggedUser.seguidores = [];
+      }
+      if (!this.centro.seguidores) {
+        this.centro.seguidores = [];
+      }
+      // agregar/remover al usuario
+      const cindex = loggedUser.seguidores.findIndex(centro => centro.id === this.centro.id);
+      if (cindex > -1) {
+        loggedUser.seguidores.splice(cindex, 1);
+      } else {
+        loggedUser.seguidores.push({ id: this.centro.id, nombre: this.centro.nombre,
+          descripcion: this.centro.descripcion, imagen: this.centro.imagen });
+      }
+      // agregar/remover al centro
+      const uindex = this.centro.seguidores.findIndex(usuario => usuario.id === loggedUser.id);
+      if (uindex > -1) {
+        this.centro.seguidores.splice(uindex, 1);
+      } else {
+        this.centro.seguidores.push({ id: loggedUser.id, nombre: loggedUser.nombre });
+      }
+    }
+  }
+
+  isFollowing(): boolean {
+    return this.authService.oUsuario &&
+      this.centro.seguidores &&
+      this.centro.seguidores.some(seguidor => seguidor.id === this.authService.oUsuario.id);
+  }
 }
