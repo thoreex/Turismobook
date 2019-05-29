ddd110b1946273a4e21f8680cb1eaf36f4edaf73
Thoreex <14024927+Thoreex@users.noreply.github.com>
2019-05-29 06:10:17 -0600

    Testing arreglado

--- src/app/centros/resenas/resenas-upsert/resenas-upsert.component.spec.ts
+++ src/app/centros/resenas/resenas-upsert/resenas-upsert.component.spec.ts
@@ -1,8 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResenasUpsertComponent } from './resenas-upsert.component';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('ResenasUpsertComponent', () => {
   let component: ResenasUpsertComponent;
@@ -11,7 +13,17 @@ describe('ResenasUpsertComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ResenasUpsertComponent ],
-      imports: [FormsModule, RouterTestingModule]
+      imports: [ReactiveFormsModule, RouterTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute, useValue: {
+            parent: {
+              snapshot: { params: of({ id: 0 }) }
+            },
+            snapshot: { params: of({ id: 0 }) }
+          }
+        }
+      ]
     })
     .compileComponents();
   }));
