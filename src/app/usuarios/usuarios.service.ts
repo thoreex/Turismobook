3959961a27aab8a2ab8aac294e332503c4be0434
Thoreex <14024927+Thoreex@users.noreply.github.com>
2019-06-08 18:29:25 -0600

    Feature de usuarios con firebase

--- src/app/usuarios/usuarios.service.ts
+++ src/app/usuarios/usuarios.service.ts
@@ -1,20 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Usuario } from './usuario';
 import { Observable, of } from 'rxjs';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
-  USUARIOS = [];
+  private collection: AngularFirestoreCollection<Usuario>;
 
-  constructor() { }
-
-  getUsuarios = (): Observable<Usuario[]> => {
-    return of(this.USUARIOS);
+  constructor(private readonly db: AngularFirestore) {
+    this.collection = this.db.collection<Usuario>('usuarios');
   }
 
-  getUsuario = (id: number): Observable<Usuario> => {
-    return of(this.USUARIOS.find(usuario => usuario.id === id));
+  getUsuarios = (): Observable<Usuario[]> => {
+    return this.collection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Usuario;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+  }
+
+  getUsuario = (id: string): Observable<Usuario> => {
+    return this.collection.doc<Usuario>(id).snapshotChanges().pipe(
+      map(a => {
+        const data = a.payload.data() as Usuario;
+        return { id, ...data };
+      })
+    );
   }
 }
