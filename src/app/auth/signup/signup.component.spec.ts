b429a0b57a5481c7f30bf0afa12532ee4a993f02
Thoreex <14024927+Thoreex@users.noreply.github.com>
2019-06-12 19:02:50 -0600

    Fix test icons

--- src/app/auth/signup/signup.component.spec.ts
+++ src/app/auth/signup/signup.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupComponent } from './signup.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
@@ -8,7 +9,8 @@ describe('SignupComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignupComponent ]
+      declarations: [ SignupComponent ],
+      imports: [ FontAwesomeModule ]
     })
     .compileComponents();
   }));
