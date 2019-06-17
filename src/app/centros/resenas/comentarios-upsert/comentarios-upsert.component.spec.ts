571a9fc0c67ad4a125138d7f8736fd78d3acb58c
Thoreex <14024927+Thoreex@users.noreply.github.com>
2019-06-16 21:05:32 -0600

    Test arreglados para comentarios

--- src/app/centros/resenas/comentarios-upsert/comentarios-upsert.component.spec.ts
+++ src/app/centros/resenas/comentarios-upsert/comentarios-upsert.component.spec.ts
@@ -1,14 +1,61 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ComentariosUpsertComponent } from './comentarios-upsert.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of, BehaviorSubject } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 describe('ComentariosUpsertComponent', () => {
   let component: ComentariosUpsertComponent;
   let fixture: ComponentFixture<ComentariosUpsertComponent>;
 
+  const FireAuthStub = {
+    authState: of({ uid: 'ABC123' })
+  };
+
+  const FirestoreStub = {
+    doc: (id: string) => ({
+      snapshotChanges: () => new BehaviorSubject({
+        payload: {
+          data: () => ({
+            nombre: 'test'
+          })
+        }
+      })
+    }),
+    collection: (name: string) => ({
+      doc: (id: string) => ({
+        snapshotChanges: () => new BehaviorSubject({
+          payload: {
+            data: () => ({
+              nombre: 'test'
+            })
+          }
+        })
+      }),
+      snapshotChanges: () => new BehaviorSubject([])
+    })
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ComentariosUpsertComponent ]
+      declarations: [ ComentariosUpsertComponent ],
+      imports: [ReactiveFormsModule, RouterTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute, useValue: {
+            parent: {
+              snapshot: { params: of({ id: 0 }) }
+            },
+            snapshot: { params: of({ id: 0 }) }
+          }
+        },
+        { provide: AngularFirestore, useValue: FirestoreStub },
+        { provide: AngularFireAuth, useValue: FireAuthStub }
+      ]
     })
     .compileComponents();
   }));
--- src/app/centros/resenas/comentarios.service.spec.ts
+++ src/app/centros/resenas/comentarios.service.spec.ts
@@ -1,9 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ComentariosService } from './comentarios.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { BehaviorSubject } from 'rxjs';
 
 describe('ComentariosService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  const FirestoreStub = {
+    collection: (name: string) => ({
+      doc: (id: string) => ({
+        snapshotChanges: () => new BehaviorSubject({
+          payload: {
+            data: () => ({
+              nombre: 'test'
+            })
+          }
+        })
+      }),
+      snapshotChanges: () => new BehaviorSubject([])
+    })
+  };
+
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      { provide: AngularFirestore, useValue: FirestoreStub }
+    ]
+  }));
 
   it('should be created', () => {
     const service: ComentariosService = TestBed.get(ComentariosService);
